3749775c601c255be7406eede2b7bd7b54438fe0
bntxperses <dbikramyt@gmail.com>
2023-11-06 21:53:34 +0530

    alioth: Bringup PixelPlusUI 6.0

--- device.mk
+++ device.mk
@@ -38,7 +38,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/products/miuicamera.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     AliothFrameworks \
